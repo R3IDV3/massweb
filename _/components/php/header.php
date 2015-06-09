ec297f7062083985cf7e7fbfcf14f8e75a429c46
Reid Vender <reid.vender@me.com>
2015-06-09 15:44:42 -0400

    Add basic authentication
    Moved restricted files to a password-protected subdirectory, added
    working-directory-dependent php to header to reflect this change, and
    added .htaccess and .htpasswd to the restricted directory to enable
    basic authentication.

--- _/components/php/header.php
+++ _/components/php/header.php
@@ -1,14 +1,28 @@
 <div class="col col-lg-12">
 	<header class="clearfix">
 		<section id="branding">
-			<a href="index.php"><img class="responsive" src="images/misc/massbase_logo.png" width="250px" vspace="5" alt="Logo for MassBase"></a>
+			<a href="index.php"><img class="responsive" src="<?php if (strpos(getcwd(), "restricted") !== FALSE) { echo "../"; } ?>images/misc/massbase_logo.png" width="250px" vspace="5" alt="Logo for MassBase"></a>
 		</section><!-- branding -->
 		<section class="navbar navbar-default"> 
 			<ul class="nav navbar-nav">
-				<li><a href="index.php">Home</a></li>
-				<li><a href="search.php">Search</a></li>
-				<li><a href="browse.php">Browse</a></li>
-				<li><a href="import.php">Import</a></li>
+				<?php 
+					if (strpos(getcwd(), "restricted") === FALSE) { // if not in the restricted directory
+						$navItems = <<<EOD
+<li><a href="index.php">Home</a></li>
+<li><a href="search.php">Search</a></li>
+<li><a href="browse.php">Browse</a></li>
+<li><a href="restricted/import.php">Import</a></li>
+EOD;
+					} else { // in the restricted directory
+						$navItems = <<<EOD
+<li><a href="../index.php">Home</a></li>
+<li><a href="../search.php">Search</a></li>
+<li><a href="../browse.php">Browse</a></li>
+<li><a href="import.php">Import</a></li>
+EOD;
+					}
+					echo "$navItems";
+				?>
 			</ul><!-- nav -->
 		</section><!-- navbar -->
 	</header>
--- _/js/myscript.js
+++ _/js/myscript.js
@@ -1,1 +1,8 @@
-$(function(){$("#home a:contains('Home')").parent().addClass("active");$("#search a:contains('Search')").parent().addClass("active");$("#browse a:contains('Browse')").parent().addClass("active")});
+$(function() {
+
+	$("#home a:contains('Home')").parent().addClass("active");
+	$("#search a:contains('Search')").parent().addClass("active");
+	$("#browse a:contains('Browse')").parent().addClass("active");
+	$("#import a:contains('Import')").parent().addClass("active");
+	
+});
