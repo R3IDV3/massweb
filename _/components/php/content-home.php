5570c2ee6f4caf499a9fab56a54a36e358c3bace
Reid Vender <reid.vender@me.com>
2015-06-25 16:13:07 -0400

    Redesign content-home

--- _/components/php/content-home.php
+++ _/components/php/content-home.php
@@ -1,13 +1,15 @@
-<article id="home">
-	<legend><h2>Welcome to MassBase</h2></legend>
-	<p>
-		MassBase is an online repository of body mass values for members of the Animal Kingdom. While body mass is a critically important life history trait that covaries and scales with a suite of other variables, it is surprisingly difficult to find verified data outside a few well-studied, charismatic groups. 
+<article id="about">
+	<h2>
+		While body mass is a critically important life history trait that covaries and scales with a suite of other variables, it is surprisingly difficult to find verified data outside a few well-studied, charismatic groups. 
 		MassBase aims to collate reliable body mass data across the whole spectrum of animal life, from snails to whales, lice to mice.
-
-	<p>
-		<?php include 'content-massbaseStats.php'; ?>
-	</p>
+	</h2>
+	<div class="column-left">
+		<h3>Large and diverse specimen archive</h3>
+		<p>The large and diverse specimen archive at BIO allow us access to a huge data source for a variety of research interests. As biodiversity scientists, we are striving to maximize the useful information that can be extracted from each specimen and species in our collection, using our available resources. </p>
+		<a class="button" href="about.php">Learn More</a>
+	</div>
+	<div class="column-right">
+		<h3>When body mass matters</h3>
+		<p>Body mass is arguably the most critical variable one can measure for biodiversity, as the number of ecological, physiological, and evolutionary characteristics it covaries with, scales by, or predicts are immense (<a href="http://books.google.ca/books/about/The_Ecological_Implications_of_Body_Size.html?id=OYVxiZgTXWsC" target="_blank">Peters 1983;</a> <a href="http://books.google.ca/books/about/Scaling.html?id=8WkjD3L_avQC&redir_esc=y" target="_blank">Schmidt-Neilsen 1984;</a> <a href="http://www.jstor.org/discover/10.2307/2097086?uid=2129&uid=2&uid=70&uid=4&sid=21104632676863" target="_blank">LaBarbera 1989;</a> <a href="http://books.google.ca/books/about/Size_Function_and_Life_History.html?id=-iBS6-2OO3wC&redir_esc=y" target="_blank">Calder 1996</a>). A massive variance in body mass exists across the Animal Kingdom (at least 15 orders of magnitude), and even between and within closely related species, so explaining the origin and maintenance of this variation is a critical objective of biodiversity science.</p>
+	</div>
 </article>
-<div>
-  <p><a class="btn btn-med btn-danger" href="search.php" role="button">Search MassBase</a></p>
-</div><!-- button -->
