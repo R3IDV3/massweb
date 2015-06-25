07c90fded5c478e2751c9d43ed4c7bd819b58bf1
Reid Vender <reid.vender@me.com>
2015-06-25 16:10:28 -0400

    Redesign header

--- _/components/php/header.php
+++ _/components/php/header.php
@@ -1,29 +1,22 @@
-<div class="col col-lg-12">
-	<header class="clearfix">
-		<section id="branding">
-			<a href="index.php"><img class="responsive" src="<?php if (strpos(getcwd(), "restricted") !== FALSE) { echo "../"; } ?>images/misc/massbase_logo.png" width="250px" vspace="5" alt="Logo for MassBase"></a>
-		</section><!-- branding -->
-		<section class="navbar navbar-default"> 
-			<ul class="nav navbar-nav">
-				<?php 
-					if (strpos(getcwd(), "restricted") === FALSE) { // if not in the restricted directory
-						$navItems = <<<EOD
-<li><a href="index.php">Home</a></li>
-<li><a href="search.php">Search</a></li>
-<li><a href="browse.php">Browse</a></li>
-<li><a href="restricted/import.php">Import</a></li>
-EOD;
-					} else { // in the restricted directory
-						$navItems = <<<EOD
-<li><a href="../index.php">Home</a></li>
-<li><a href="../search.php">Search</a></li>
-<li><a href="../browse.php">Browse</a></li>
-<li><a href="import.php">Import</a></li>
-EOD;
-					}
-					echo "$navItems";
-				?>
-			</ul><!-- nav -->
-		</section><!-- navbar -->
-	</header>
-</div><!-- column -->	
+<header>
+	<div id="logo">
+		<div class="component">
+			<!-- Weight icon by Icons8 -->
+			<img src="_/img/mb.png" title="Weight">
+		</div>
+		<div class="component">
+			MassBase
+		</div>
+	</div>
+	<nav>
+		<ul>
+			<li><a href="#">Home</a></li>
+			<li><a href="#about">About</a></li>
+			<li><a href="#search">Search</a></li>
+			<li><a href="#">Browse</a></li>
+		</ul>
+	</nav>
+	<div id="feature">
+		<h1>An online repository of body mass values for members of the Animal Kingdom.</h1>
+	</div>
+</header>
